b065b6b44060929978f36c112ae6e9f9e715b45d
Julius Gonsior <julius@gaenseri.ch>
2020-08-31 16:53:23 +0200

    fix

--- activeLearner.py
+++ activeLearner.py
@@ -78,8 +78,7 @@ class ActiveLearner:
             self.data_storage.train_labeled_X,
             self.data_storage.train_labeled_Y["label"].to_list(),
             sample_weight=compute_sample_weight(
-                "balanced",
-                self.data_storage.train_labeled_Y["label"].to_list(),
+                "balanced", self.data_storage.train_labeled_Y["label"].to_list(),
             ),
         )
 
@@ -218,16 +217,17 @@ class ActiveLearner:
                 log_it("Budget exhausted")
                 if not ALLOW_RECOMMENDATIONS_AFTER_STOP:
                     break
-            if kwargs["STOP_AFTER_MAXIMUM_ACCURACY_REACHED"]:
-                if (
-                    self.metrics_per_al_cycle["test_acc"][-1]
-                    >= kwargs["THEORETICALLY_BEST_ACHIEVABLE_ACCURACY"]
-                ):
-                    early_stop_reached = True
-                    print(
-                        "THEORETICALLY_BEST_ACHIEVABLE_ACCURACY: "
-                        + str(kwargs["THEORETICALLY_BEST_ACHIEVABLE_ACCURACY"])
-                    )
-                    break
+            if "STOP_AFTER_MAXIMUM_ACCURACY_REACHED" in kwargs:
+                if kwargs["STOP_AFTER_MAXIMUM_ACCURACY_REACHED"]:
+                    if (
+                        self.metrics_per_al_cycle["test_acc"][-1]
+                        >= kwargs["THEORETICALLY_BEST_ACHIEVABLE_ACCURACY"]
+                    ):
+                        early_stop_reached = True
+                        print(
+                            "THEORETICALLY_BEST_ACHIEVABLE_ACCURACY: "
+                            + str(kwargs["THEORETICALLY_BEST_ACHIEVABLE_ACCURACY"])
+                        )
+                        break
 
         return (self.clf, self.metrics_per_al_cycle)
