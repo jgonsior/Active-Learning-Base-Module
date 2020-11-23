e3e685d1b8ed6001c5add2dd43fbc67cb97f3eee
Julius Gonsior <julius@gaenseri.ch>
2020-11-23 18:43:29 +0100

    added trained_batch_nn

---
+++ sampling_strategies/trained_batch_nn.py
@@ -0,0 +1,32 @@
+import os
+
+import dill
+import numpy as np
+
+from .learnedBaseSampling import LearnedBaseSampling
+
+
+class TrainedNNLearner(LearnedBaseSampling):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+        with open(kwargs["NN_BINARY_PATH"], "rb") as handle:
+            model = dill.load(handle)
+
+        self.sampling_classifier = model
+
+    def get_X_query_index(self):
+        return self.sample_unlabeled_X(
+            self.sampling_classifier.n_outputs_,
+            INITIAL_BATCH_SAMPLING_ARG=self.INITIAL_BATCH_SAMPLING_ARG,
+            INITIAL_BATCH_SAMPLING_METHOD=self.INITIAL_BATCH_SAMPLING_METHOD,
+        )
+
+    def get_sorting(self, X_state):
+        X_state = np.reshape(X_state, (1, len(X_state)))
+        Y_pred = self.sampling_classifier.predict(X_state)
+        sorting = Y_pred
+        return sorting
