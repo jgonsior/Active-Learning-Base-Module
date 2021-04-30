e6330f381bf1bdbd29d83e703817502e613d7f76
Julius Gonsior <julius@gaenseri.ch>
2021-04-30 15:14:41 +0200

    back to icjai synthetic datasets?

--- datasets/synthetic.py
+++ datasets/synthetic.py
@@ -18,90 +18,36 @@ def load_synthetic(
     no_valid_synthetic_arguments_found = True
     while no_valid_synthetic_arguments_found:
         log_it("Start finding synthetic dataset")
-        if not NEW_SYNTHETIC_PARAMS:
-            if VARIABLE_DATASET:
-                N_SAMPLES = random.randint(100, 5000)
-            else:
-                N_SAMPLES = random.randint(100, 2000)
+        N_SAMPLES = random.randint(100, 5000)
+        N_FEATURES = random.randint(2, 100)
 
-            if AMOUNT_OF_FEATURES > 0:
-                N_FEATURES = AMOUNT_OF_FEATURES
-            else:
-                N_FEATURES = random.randint(2, 100)
+        N_INFORMATIVE, N_REDUNDANT, N_REPEATED = [
+            int(N_FEATURES * i)
+            for i in np.random.dirichlet(np.ones(3), size=1).tolist()[0]  # type: ignore
+        ]
 
-            N_INFORMATIVE, N_REDUNDANT, N_REPEATED = [
-                int(N_FEATURES * i)
-                for i in np.random.dirichlet(np.ones(3), size=1).tolist()[0]  # type: ignore
-            ]
+        N_CLASSES = random.randint(2, 10)
+        N_CLUSTERS_PER_CLASS = random.randint(
+            1, min(max(1, int(2 ** N_INFORMATIVE / N_CLASSES)), 10)
+        )
+        if N_CLASSES * N_CLUSTERS_PER_CLASS > 2 ** N_INFORMATIVE:
+            continue
+        no_valid_synthetic_arguments_found = False
 
-            # N_REPEATED = 0
+        WEIGHTS = np.random.dirichlet(np.ones(N_CLASSES), size=1).tolist()[
+            0
+        ]  # list of weights, len(WEIGHTS) = N_CLASSES, sum(WEIGHTS)=1
 
-            N_CLASSES = random.randint(2, 10)
-            # N_CLUSTERS_PER_CLASS = random.randint(1, 10)
-            N_CLUSTERS_PER_CLASS = random.randint(
-                1, min(max(1, int(2 ** N_INFORMATIVE / N_CLASSES)), 10)
-            )
-            if N_CLASSES * N_CLUSTERS_PER_CLASS > 2 ** N_INFORMATIVE:
-                continue
-            no_valid_synthetic_arguments_found = False
+        FLIP_Y = (
+            np.random.pareto(2.0) + 1  # type: ignore
+        ) * 0.01  # amount of noise, larger values make it harder
 
-            WEIGHTS = np.random.dirichlet(np.ones(N_CLASSES), size=1).tolist()[
-                0
-            ]  # list of weights, len(WEIGHTS) = N_CLASSES, sum(WEIGHTS)=1
+        CLASS_SEP = random.uniform(
+            0, 10
+        )  # larger values spread out the clusters and make it easier
+        HYPERCUBE = False  # if false random polytope
+        SCALE = 0.01  # features should be between 0 and 1 now
 
-            if GENERATE_NOISE:
-                FLIP_Y = (
-                    np.random.pareto(2.0) + 1  # type: ignore
-                ) * 0.01  # amount of noise, larger values make it harder
-            else:
-                FLIP_Y = 0
-
-            CLASS_SEP = random.uniform(
-                0, 10
-            )  # larger values spread out the clusters and make it easier
-            HYPERCUBE = HYPERCUBE  # if false random polytope
-            SCALE = 0.01  # features should be between 0 and 1 now
-        else:
-            if VARIABLE_DATASET:
-                N_SAMPLES = random.randint(100, 5000)
-            else:
-                N_SAMPLES = random.randint(100, 2000)
-                #  N_SAMPLES = 1000
-            if AMOUNT_OF_FEATURES > 0:
-                N_FEATURES = AMOUNT_OF_FEATURES
-            else:
-                N_FEATURES = random.randint(2, 20)
-            N_REDUNDANT = N_REPEATED = 0
-            N_INFORMATIVE = N_FEATURES
-
-            N_CLASSES = random.randint(2, min(10, 2 ** N_INFORMATIVE))
-            N_CLUSTERS_PER_CLASS = random.randint(
-                1, int(2 ** N_INFORMATIVE / N_CLASSES)
-            )
-
-            if N_CLASSES * N_CLUSTERS_PER_CLASS > 2 ** N_INFORMATIVE:
-                print("ui")
-                continue
-            no_valid_synthetic_arguments_found = False
-
-            WEIGHTS = np.random.dirichlet(np.ones(N_CLASSES), size=1).tolist()[
-                0
-            ]  # list of weights, len(WEIGHTS) = N_CLASSES, sum(WEIGHTS)=1
-
-            if GENERATE_NOISE:
-                FLIP_Y = (
-                    np.random.pareto(2.0) + 1  # type: ignore
-                ) * 0.01  # amount of noise, larger values make it harder
-            else:
-                FLIP_Y = 0
-
-            CLASS_SEP = random.uniform(
-                0, 10
-            )  # larger values spread out the clusters and make it easier
-
-            HYPERCUBE = HYPERCUBE  # if false a random polytope is selected instead
-            SCALE = 0.01  # features should be between 0 and 1 now
-    # N_SAMPLES = 80
     synthetic_creation_args = {
         "n_samples": N_SAMPLES,  # type: ignore
         "n_features": N_FEATURES,  # type: ignore
--- datasets/uci.py
+++ datasets/uci.py
@@ -8,7 +8,22 @@ def load_uci(
 ) -> Tuple[pd.DataFrame, Dict[str, Any]]:
     df = pd.read_csv(DATASETS_PATH + "/uci_cleaned/" + DATASET_NAME + ".csv")
 
-    synthetic_creation_args = {}
+    synthetic_creation_args = synthetic_creation_args = {
+        "n_samples": len(df),
+        "n_features": len(df.columns),  # type: ignore
+        "n_informative": len(df.columns),  # type: ignore
+        "n_redundant": 0,  # type: ignore
+        "n_repeated": 0,  # type: ignore
+        "n_classes": len(df["LABEL"].unique()),  # type: ignore
+        "n_clusters_per_class": "?",  # type: ignore
+        "weights": "?",  # type: ignore
+        "flip_y": "?",  # type: ignore
+        "class_sep": "?",  # type: ignore
+        "hypercube": "?",
+        "scale": "?",  # type: ignore
+        "random_state": RANDOM_SEED,
+    }
+
     synthetic_creation_args["n_classes"] = len(df["LABEL"].unique())
 
     Y = df["LABEL"].to_numpy()
